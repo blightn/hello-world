af04cf20e5bdcc9c0f385b64b7cf30f5c86b94e8
blightn <blightan@gmail.com>
2021-03-01 10:09:59 -0800

    some fixes

--- mytest/firstapp/views.py
+++ mytest/firstapp/views.py
@@ -6,36 +6,37 @@ from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from django.views.generic import ListView, DetailView
 from django.urls import reverse_lazy
 from django.contrib.auth.mixins import LoginRequiredMixin
+from django.shortcuts import redirect
 from .models import Note
 from .forms import FirstAppForm
 
 
-class FirstAppCreateView(LoginRequiredMixin, CreateView):
-    # Создание записки
-
+class CustomLoginRequiredMixin(LoginRequiredMixin):
     login_url = '/login/'
 
+class FirstAppCreateView(CustomLoginRequiredMixin, CreateView):
+    # Создание записки
+
     model = Note
-    success_url = reverse_lazy('firstapp:list')
     template_name = 'firstapp/operations.html'
-    form_class = FirstAppForm
-    #fields = '__all__'
-    
+    #form_class = FirstAppForm  # get_context_data() got an unexpected keyword argument 'form'
+    #fields = '__all__'         # get_context_data() got an unexpected keyword argument 'form'
+    fields = ('title', 'text')
+
     def form_valid(self, form):
         # This method is called when valid form data has been POSTed. It should return an HttpResponse.
+        form.instance.author = self.request.user
         if form.is_valid():
             form.save()
-        return super().form_valid(form)
-
+        return redirect('firstapp:list')
+    
     def get_context_data(self):
         context = super().get_context_data()
         context['action'] = 'Добавить записку'
         return context
 
-class FirstAppListView(LoginRequiredMixin, ListView):
+class FirstAppListView(CustomLoginRequiredMixin, ListView):
     # Просмотр записок
-
-    login_url = '/login/'
 
     model = Note
     template_name = 'firstapp/list.html'
@@ -46,10 +47,8 @@ class FirstAppListView(LoginRequiredMixin, ListView):
         self.object = Note.objects.filter(author=self.request.user)
         return self.object
 
-class FirstAppDetailView(LoginRequiredMixin, DetailView):
+class FirstAppDetailView(CustomLoginRequiredMixin, DetailView):
     # Просмотр деталей записки
-
-    login_url = '/login/'
 
     model = Note
     template_name = 'firstapp/details.html'
@@ -57,34 +56,23 @@ class FirstAppDetailView(LoginRequiredMixin, DetailView):
     form_class = FirstAppForm
     #fields = '__all__'
 
-    def get_queryset(self):
-        self.object = Note.objects.filter(author=self.request.user)
-        return self.object
-
-class FirstAppUpdateView(LoginRequiredMixin, UpdateView):
+class FirstAppUpdateView(CustomLoginRequiredMixin, UpdateView):
     # Изменение записки
-
-    login_url = '/login/'
 
     model = Note
     success_url = reverse_lazy('firstapp:list')
     template_name = 'firstapp/operations.html'
-    form_class = FirstAppForm
-    #fields = '__all__'
+    #form_class = FirstAppForm  # get_context_data() got an unexpected keyword argument 'form'
+    #fields = '__all__'         # get_context_data() got an unexpected keyword argument 'form'
+    fields = ('title', 'text')
 
     def get_context_data(self):
         context = super().get_context_data()
         context['action'] = 'Изменить записку'
         return context
 
-    def get_queryset(self):
-        self.object = Note.objects.filter(author=self.request.user)
-        return self.object
-
-class FirstAppDeleteView(LoginRequiredMixin, DeleteView):
+class FirstAppDeleteView(CustomLoginRequiredMixin, DeleteView):
     # Удаление записки
-
-    login_url = '/login/'
 
     model = Note
     success_url = reverse_lazy('firstapp:list')
